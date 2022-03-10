226d86f94cf0a1d927cad6312227693ee9505baf
kei-s16 <kei1216s@nifty.com>
2022-03-11 00:02:05 +0900

    cp .config/nvim/rc/basic.vim .config/nvim/lua/options.lua

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,37 @@
+scriptencoding utf-8
+
+set tabstop=8
+set noexpandtab
+set autoindent
+set smartindent
+set backspace=indent,eol,start
+set wrapscan
+set showmatch
+set wildmenu
+set formatoptions+=mM
+
+set number
+set cursorline
+set ruler
+set list
+set listchars=tab:>-,extends:<,trail:-,eol:<
+set wrap
+set laststatus=2
+set cmdheight=1
+set showcmd
+set title
+
+set hidden
+
+set encoding=utf-8
+set fileencodings=utf-8,iso-20222-jp,euc-jp,sjis
+set fileformats=unix,dos,mac
+
+set ambiwidth=double
+set showbreak=->\ 
+
+set completeopt-=preview
+
+set foldmethod=indent
+
+set inccommand=split
