762cc0adff53e06b12177286b445048c2d09713a
kei-s16 <kei1216s@nifty.com>
2022-10-31 22:23:04 +0900

    公式ドキュメントベースに外部フォントに頼らないように

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,7 +1,36 @@
 require("noice").setup({
-    presets = {
-      bottom_search = true,
-      command_palette = false,
-      long_message_to_split = false,
-    }
+  presets = {
+    bottom_search = true,
+    command_palette = false,
+    long_message_to_split = false,
+  },
+  views = {
+    cmdline_popup = {
+      border = {
+        style = "none",
+        padding = { 2, 3 },
+      },
+      filter_options = {},
+      win_options = {
+        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+      },
+    },
+  },
+  cmdline = {
+    format = {
+      cmdline = { pattern = "^:", icon = ":", lang = "vim" },
+      search_down = { kind = "search", pattern = "^/", icon = "/", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", icon = "?", lang = "regex" },
+    },
+  },
+  routes = {
+    {
+      filter = {
+        event = "msg_show",
+        kind = "",
+        find = "written",
+      },
+      opts = { skip = true },
+    },
+  },
 })
